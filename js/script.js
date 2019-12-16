b41a6170a391b54fab6d9658f754cb7e276119f4
Majid Alfredi <majidalfredi123@gmail.com>
2019-12-16 20:53:42 +0700

    memperbaiki pada file script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-function showAllMenu() {
-    $.getJSON('data/pizza.json', function(result){
-        let menu = result.menu;
-        $('#daftar-menu').empty();
-        $.each(menu, function(i, result){
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + result.gambar + '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + result.nama + '</h5><p class="card-text">' + result.deskripsi + '</p><h5 class="card-title">Rp. ' + result.harga  + ',-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
-        });
-    });
+function tampilkanSemuaMenu(){
 
+    $.getJSON('data/pizza.json', function(data){
+        let menu = data.menu;
+        $.each(menu, function(i, data){
+            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3" style="width: 18rem;"><img src="img/menu/'+ data.gambar +'" class="card-img -top" alt=""><div class="card- body"><h5 class="card-title">'+ data.nama +'</h5><h5 class="card-title">Rp. '+ data.harga +'</h5><p class="card-text">'+ data.deskripsi +'</p><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+        });
+    
+    });
 }
 
-showAllMenu();
+tampilkanSemuaMenu();
 
-$('.nav-link').on('click', function() {
+$('.nav-link').on('click', function(){
     $('.nav-link').removeClass('active');
     $(this).addClass('active');
 
-    let category = $(this).html();
-    $('h1').html(category);
+    let kategori = $(this).html();
+    $('h1').html(kategori);
 
-    if( category == 'All Menu' ){
-        showAllMenu();
+    if (kategori == 'All Menu') {
+        tampilkanSemuaMenu();
         return;
     }
 
-    $.getJSON('data/pizza.json', function(result) {
-        let menu = result.menu;
+    $.getJSON('data/pizza.json', function(data){
+        let menu = data.menu;
         let content = '';
 
-        $.each(menu, function(i, result){
-            if( result.category == category.toLowerCase()) {
-                content += '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + result.gambar + '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' + result.nama + '</h5><p class="card-text">' + result.deskripsi + '</p><h5 class="card-title">Rp. ' + result.harga  + ',-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
+        $.each(menu, function(i, data){
+            if (data.kategori == kategori.toLowerCase()) {
+                content += '<div class="col-md-4"><div class="card mb-3" style="width: 18rem;"><img src="img/menu/'+ data.gambar +'" class="card-img -top" alt=""><div class="card- body"><h5 class="card-title">'+ data.nama +'</h5><h5 class="card-title">Rp. '+ data.harga +'</h5><p class="card-text">'+ data.deskripsi +'</p><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
             }
         });
 
-        $('#daftar-menu').html(content);    
+        $('#daftar-menu').html(content);
     });
+
 });
